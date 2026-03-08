aff978a375fb13b4f9429d75c8be74b1c63f7a8c
Burhan Burak <140156325+Burrhanburak@users.noreply.github.com>
2026-03-08 06:21:49 +0300

    depoy

--- components/home/DeferredSections.tsx
+++ components/home/DeferredSections.tsx
@@ -3,10 +3,17 @@
 import dynamic from "next/dynamic";
 import DeferredMount from "@/components/DeferredMount";
 
-const ElectronBento = dynamic(() => import("@/components/ElectronBento"), { ssr: false });
+const loadElectronBento = () => import("@/components/ElectronBento");
+const ElectronBento = dynamic(() => loadElectronBento(), { ssr: false });
+
 export function DeferredElectronBento() {
   return (
-    <DeferredMount fallback={<div className="h-[600px] w-full" />} rootMargin="800px 0px">
+    <DeferredMount
+      fallback={<div className="h-[600px] w-full" />}
+      rootMargin="800px 0px"
+      prefetch={loadElectronBento}
+      prefetchMargin="1200px 0px"
+    >
       <ElectronBento />
     </DeferredMount>
   );
